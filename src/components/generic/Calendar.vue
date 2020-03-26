5dfb3f7d7f303741087d1097f5c285a693dcaaee
Grégoire Lannoy <gregoire.lannoy@gmail.com>
2020-03-26 19:08:37 +0100

    Won't work without those files

---
+++ src/components/generic/Calendar.vue
@@ -0,0 +1,93 @@
+<template>
+  <q-calendar
+    v-model="selectedDate"
+    view="week"
+    :weekdays="[1, 2, 3, 4, 5]"
+    interval-start="6"
+    interval-count="14"
+    hour24-format>
+    <template #day-body="{ day, timeStartPos, timeDurationHeight }">
+      <template v-for="(event, key) in events(day)">
+        <calendar-event :key="key"
+          class="absolute rotate-90"
+          :event="event"
+          :style="eventStyle(event, timeStartPos, timeDurationHeight)"
+          @click="$emit('editTask', event.task)" />
+      </template>
+    </template>
+  </q-calendar>
+</template>
+
+<script>
+import moment from 'moment'
+
+export default {
+  name: 'Calendar',
+  data () {
+    return {
+      selectedDate: ''
+    }
+  },
+  methods: {
+    events (day) {
+      let busyRow = []
+
+      const tasks = this.$store.getters['tasks/filter']()
+      let events = []
+      tasks.forEach(t => {
+        if (t.time_shifts !== undefined) {
+          t.time_shifts.forEach(s => {
+            if (parseInt(moment(s.start).format('DD')) === day) {
+              // Calculate if row is busy
+              let row = 0
+              let placed = true
+              while (placed) {
+                if (busyRow[row] === undefined) busyRow[row] = []
+                if (this.rowIsBusy(busyRow[row], s)) row++
+                else {
+                  busyRow[row].push(s)
+                  placed = false
+                }
+              }
+
+              events.push({
+                row: row,
+                time: moment(s.start).format('HH:mm'),
+                duration: moment(s.end).diff(moment(s.start), 'minutes'),
+                icon: 'add',
+                task: t
+              })
+            }
+          })
+        }
+      })
+      return events
+    },
+    eventStyle (event, timeStartPos, timeDurationHeight) {
+      let s = {}
+      s.top = timeStartPos(event.time) + 'px'
+      s.left = `${28 * (event.row + 1)}px`
+      s.width = timeDurationHeight(event.duration) + 'px'
+      s['max-width'] = timeDurationHeight(event.duration) + 'px'
+      s['transform-origin'] = 'top left'
+      return s
+    },
+    rowIsBusy (busyRow, s) {
+      let busy = false
+      busyRow.forEach(busyTime => {
+        if (moment(s.start).isBetween(moment(busyTime.start), moment(busyTime.end))) busy = true
+        if (moment(s.end).isBetween(moment(busyTime.start), moment(busyTime.end))) busy = true
+        if (moment(s.start).isSame(moment(busyTime.start))) busy = true
+      })
+      return busy
+    },
+    openEditDrawer (task) {
+      this.drawer = true
+      this.taskToEdit = task
+    }
+  },
+  components: {
+    CalendarEvent: () => import('./CalendarEvent')
+  }
+}
+</script>
---
+++ src/components/generic/StoreToolbar.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="row justify-end">
+    <q-btn v-for="(action, key) in toolbarActions" :key="key"
+      dense
+      v-bind="action.props"
+      @click="triggerAction(action)" />
+
+    <q-btn v-if="get('selectedIds').length > 0"
+      icon="more_vert"
+      flat round dense
+      @click="bottomSheet" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'StoreToolbar',
+  props: {
+    store: {
+      type: String,
+      default: '',
+      required: true
+    }
+  },
+  data () {
+    return {
+      editDialog: false
+    }
+  },
+  computed: {
+    toolbarActions () {
+      return this.get('toolbarActions').filter(a => a.customFilter(this))
+    }
+  },
+  methods: {
+    get (getter) {
+      return this.$store.getters[this.store + '/' + getter]
+    },
+    bottomSheet () {
+      this.$q.bottomSheet({
+        message: 'Actions',
+        actions: this.get('actions').filter(a => !a.toolbar)
+      }).onOk(action => {
+        this.triggerAction(action)
+      })
+    },
+    triggerAction (action) {
+      const selectedIds = this.get('selectedIds')
+      if (action.patch) {
+        let payload = {
+          doc: action.patch,
+          ids: selectedIds
+        }
+        this.$store.dispatch(this.store + '/patchBatch', payload)
+      } else if (action.action) {
+        let payload = {
+          parent: this,
+          ids: selectedIds
+        }
+        this.$store.dispatch(this.store + '/' + action.action, payload)
+      }
+    }
+  }
+}
+</script>
