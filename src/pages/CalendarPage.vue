5b94bc5c6f83de3755025218d659507ee9085cd1
Grégoire Lannoy <gregoire.lannoy@gmail.com>
2020-03-26 21:15:14 +0100

    Won't work without this

---
+++ src/pages/CalendarPage.vue
@@ -0,0 +1,56 @@
+<template>
+  <q-layout container view="hHh Lpr lff" style="height: calc(100vh - 50px)">
+    <q-drawer v-model="drawer" side="left" bordered behavior="mobile">
+      <q-scroll-area class="fit q-pa-sm">
+        <store-form v-model="taskToEdit" store="tasks" @submit="drawer = false">
+          <template #buttons>
+            <q-btn v-if="taskToEdit === undefined"
+              label="Reset"
+              type="reset"
+              :color="$tasks.meta.color"
+              flat
+              class="q-ml-sm" />
+            <q-btn
+              :label="taskToEdit ? 'Save' : 'Create'"
+              type="submit"
+              :color="$tasks.meta.color" />
+          </template>
+        </store-form>
+      </q-scroll-area>
+    </q-drawer>
+
+    <q-page-container>
+      <q-page>
+        <calendar @task-click="openEditDrawer" />
+      </q-page>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<style>
+.q-current-day {
+  background-color: white;
+}
+</style>
+
+<script>
+export default {
+  name: 'CalendarPage',
+  data () {
+    return {
+      taskToEdit: undefined,
+      drawer: false
+    }
+  },
+  methods: {
+    openEditDrawer (task) {
+      this.drawer = true
+      this.taskToEdit = task
+    }
+  },
+  components: {
+    Calendar: () => import('../components/generic/Calendar'),
+    StoreForm: () => import('../components/generic/StoreForm')
+  }
+}
+</script>
